3a438510eb9e78163d1268dc5e9740beb22f6266
mitra <84022001+fatemebd@users.noreply.github.com>
2023-04-25 01:29:00 +0430

    changes

---
+++ src/pages/manager_mainpage/components/Header.js
@@ -0,0 +1,48 @@
+import { useState, useCallback } from "react";
+import ManagerSide from "./ManagerSide";
+import PortalPopup from "./PortalPopup";
+import styles from "./Header.module.css";
+const Header = () => {
+  const [isManagerSideOpen, setManagerSideOpen] = useState(false);
+
+  const openManagerSide = useCallback(() => {
+    setManagerSideOpen(true);
+  }, []);
+
+  const closeManagerSide = useCallback(() => {
+    setManagerSideOpen(false);
+  }, []);
+
+  return (
+    <>
+      <div className={styles.header}>
+        <img
+          className={styles.universityOfIsfahanLogo2Icon}
+          alt=""
+          src="/university-of-isfahanlogo-2@2x.png"
+        />
+        <div className={styles.wrapper}>
+          <b className={styles.b}>
+            سامانه ارزیابی کارکنان دانشکده مهندسی کامپیوتر
+          </b>
+        </div>
+        <div className={styles.accountWrapper} onClick={openManagerSide}>
+          <div className={styles.account}>
+            <img className={styles.phlistIcon} alt="" src="/phlist.svg" />
+          </div>
+        </div>
+      </div>
+      {isManagerSideOpen && (
+        <PortalPopup
+          overlayColor="rgba(113, 113, 113, 0.3)"
+          placement="Centered"
+          onOutsideClick={closeManagerSide}
+        >
+          <ManagerSide onClose={closeManagerSide} />
+        </PortalPopup>
+      )}
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/manager_mainpage/components/Table.js
@@ -0,0 +1,34 @@
+import styles from "./Table.module.css";
+const data = [
+  { name: "Anom", age: 19, gender: "Male" },
+  { name: "Megha", age: 19, gender: "Female" },
+  { name: "Subham", age: 25, gender: "Male"},
+]
+const Table = () => {
+  return (
+    <div className="Table">
+      <table>
+        <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        </thead>
+        <tbody>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.name}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
