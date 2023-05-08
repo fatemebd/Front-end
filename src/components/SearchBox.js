a68140aa5cfd6fecba4078ea437a30701d4a563a
Fateme Aghababaei <faghababaei81@gmail.com>
2023-05-08 23:15:05 +0330

    search box
    add enter event to search box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,12 @@ import "../styles/search_box.css";
 const SearchBox=() =>{
     return(
         <div className="search-box">
-          <input className="input-search" type="text" placeholder="جست‌وجو در نظرسنجی‌ها"></input>
+          <input className="input-search" type="text" placeholder="جست‌وجو در نظرسنجی‌ها" 
+          onKeyDown={(e) => {if(e.key === "Enter") {
+            let tbl=document.getElementById('tbl');
+            tbl.remove();
+          }}} >
+          </input>
           <button className="search-icon">
           <img
             className="ssearch-rounde-icon"
