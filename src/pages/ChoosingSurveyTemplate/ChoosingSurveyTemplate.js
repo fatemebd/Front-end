34da0ca9e8fb6f3356a4242ab6e842739d06ad8d
Fateme Aghababaei <faghababaei81@gmail.com>
2023-05-28 12:27:34 +0330

    Choosing Survey Template
    creating choosing survey template page.

---
+++ src/pages/ChoosingSurveyTemplate/ChoosingSurveyTemplate.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from "./ChoosingSurveyTemplate.module.css";
+import Header from '../../components/Admin_Header';
+import Footer from '../../components/Footer';
+import SearchBox from "../../components/SearchBox";
+import Box from "../../components/Box"
+import StatusBar from '../../components/StatusBar';
+
+function ChoosingSurveyTemplate () {
+    const navigate = useNavigate();
+    if (localStorage.getItem('token') == 'null') {
+        navigate("/");
+        return;
+    }      
+
+    return (
+        <div className={styles.mainPage}>
+            <Header />
+            <SearchBox />
+            <div className={styles.main}>
+                <div className={styles.stat}>
+                    <StatusBar status="2" />
+                </div>
+                <div className={styles.container}>
+
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+                    <Box formName="رفتار با دانشجویان" numOfQuestions="۸" />
+
+                </div>
+
+                
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default ChoosingSurveyTemplate;
