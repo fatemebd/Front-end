f8e4fa958f55258f2d89ee3db2e0f1e29160e769
fateme <84022001+fatemebd@users.noreply.github.com>
2023-06-02 02:30:56 +0330

    Update SearchBox.js

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,76 +2,58 @@ import style from "../styles/search_box.module.css";
 import Table from "../components/Table";
 import React, { useState, useEffect } from "react";
 
+const SearchBox = (props) => {
+  const [data, setData] = useState([]);
 
-const SearchBox=(props) => {
-  
-
-  const render=(data)=>{
-    console.log(props);
-  
-    return(
-      <Table  data={data} columns={props.columns} values={props.values} apiLink={props.apilink} className={style.table} />
-    );
-  }
-
-      const [data, setData] = useState([]);
-      useEffect(() => {
-        fetch(props.apilink)
-          .then(response => response.json())
-          .then(data => setData(data))
-          .catch(error => console.error(error));
-      }, []);
-
-
-  const HandleSearch=(link,tbl,props) =>
-{
-    
-    fetch(link)
+  useEffect(() => {
+    fetch(props.apilink)
       .then(response => response.json())
-       .then(data => setData(data))
-       .catch(error => console.error(error));
-        return(<Table  data={data} columns={props.columns} values={props.values} apiLink={props.apilink} className={style.table} />)
-      //  tbl.innerHTML=`<div id="5">
-      //  <Table data={data} columns={props.columns} values={props.values} apiLink={link} className={style.table} />
-      // </div>`;
-}
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, []);
 
-    return(
-      <>
-      
-        <div className={style.search_box}>
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      const link = props.apilink + "?search=" + e.target.value;
+      fetch(link)
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.error(error));
+    }
+  };
+
+  // only render the Table component when there is data to display
+  // const tableComponent = data.length > 0 ? (
+  //   <Table data={data} columns={props.columns} values={props.values} apiLink={props.apilink} />
+  // ) : null;
+const t=()=>{
+  return(
+    <Table data={data} columns={props.columns} values={props.values} apiLink={props.apilink} />
+
+  )
+}
+  return (
+    <>
+      <div className={style.search_box}>
         <button className={style.search_icon}>
           <img
             className={style.ssearch_rounde_icon}
             alt=""
             src="assets/img/loupe.png"
           />
-          </button>
-          <input className={style.input_search} type="text" placeholder={props.text}
-          onKeyDown={(e) => 
-            {if(e.key=="Enter")
-            {
-            const link=props.apilink+"?search=" +e.target.value;
-            const tbl_div=document.getElementById("5");
-            const tbl=document.getElementById("tbl1");
-            while(tbl.firstElementChild)
-               tbl.firstElementChild.remove();
-           console.log(tbl);
-            console.log(tbl);
-            HandleSearch(link,tbl,props);
-           }
-           
-          }}
-          >
-          </input>
-         
-         
+        </button>
+        <input
+          className={style.input_search}
+          type="text"
+          placeholder={props.text}
+          onKeyDown={handleSearch}
+        />
       </div>
       <div id="tbl1" className={style.table_body}>
-      {render(data)}
-
+        {t()}
       </div>
     </>
-    );
+  );
 };
+
 export default SearchBox;
